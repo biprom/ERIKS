d585bba43453a02d552b4d513f6dc3cc7be0f87c
Kristof Taveirne <kristof.taveirne@gmail.com>
2016-09-24 18:46:36 +0200

    Fix ClassCastException on dataSource.getItemsIds(); Fix CpuConfigBean bug where bean got reused but id was not reset.

--- TelemSpring/src/main/java/com/biprom/eriks/telem/model/CpuConfigBean.java
+++ TelemSpring/src/main/java/com/biprom/eriks/telem/model/CpuConfigBean.java
@@ -1,50 +1,37 @@
 package com.biprom.eriks.telem.model;
 
-import java.util.Date;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Document
 public class CpuConfigBean {
-	
+
 	@Id
 	private String id;
-	
-	
-
 
 
 	@Indexed
 	private String cardNumber;
+
 	private String ioNumber;
+
 	private String ioName;
-
-
 
 
 	public CpuConfigBean(String cardnumb, String ionumb, String ioname) {
 		this.cardNumber = cardnumb;
 		this.ioNumber = ionumb;
 		this.ioName = ioname;
-		
-		
-	
+
 
 	}
-	
-	
+
+
 	public CpuConfigBean() {
-		
-		
-	
+
 
 	}
-
-
-
 
 
 	public String getCardNumber() {
@@ -52,13 +39,9 @@ public class CpuConfigBean {
 	}
 
 
-
-
 	public void setCardNumber(String cardNumber1) {
 		this.cardNumber = cardNumber1;
 	}
-
-
 
 
 	public String getIoNumber() {
@@ -66,20 +49,14 @@ public class CpuConfigBean {
 	}
 
 
-
-
 	public void setIoNumber(String ioNumber1) {
 		this.ioNumber = ioNumber1;
 	}
 
 
-
-
 	public String getIoName() {
 		return ioName;
 	}
-
-
 
 
 	public void setIoName(String ioName1) {
@@ -95,9 +72,6 @@ public class CpuConfigBean {
 	public void setId(String id) {
 		this.id = id;
 	}
-
-
-
 
 
 }
--- TelemSpring/src/main/java/com/biprom/eriks/telem/ui/HardwareView.java
+++ TelemSpring/src/main/java/com/biprom/eriks/telem/ui/HardwareView.java
@@ -2,7 +2,7 @@ package com.biprom.eriks.telem.ui;
 
 import com.biprom.eriks.telem.dao.CpuConfigRepository;
 import com.biprom.eriks.telem.model.CpuConfigBean;
-import com.vaadin.data.Item;
+import com.vaadin.data.Container;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.data.util.BeanItemContainer;
@@ -13,18 +13,16 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
-
-import java.util.Iterator;
-
-import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.dialogs.ConfirmDialog;
 
+import java.util.Collection;
+
 @SpringView(name = HardwareView.VIEW_NAME)
 public class HardwareView extends HardwareDesign implements View {
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
@@ -36,10 +34,10 @@ public class HardwareView extends HardwareDesign implements View {
 	public static final double minSliderValue = 0;
 
 	public static final double maxSliderValue = 8;
-	
+
 	BeanItemContainer<CpuConfigBean> beanItemContainer =
-		    new BeanItemContainer<CpuConfigBean>(CpuConfigBean.class);
-	
+			new BeanItemContainer<CpuConfigBean>(CpuConfigBean.class);
+
 
 	public HardwareView() {
 
@@ -67,7 +65,6 @@ public class HardwareView extends HardwareDesign implements View {
 				Notification.show("Attention! Sildervalue has changed to " + event.getProperty().getValue());
 
 
-				
 			}
 
 		});
@@ -86,7 +83,6 @@ public class HardwareView extends HardwareDesign implements View {
 
 				Notification.show("Attention! Sildervalue has changed to " + event.getProperty().getValue());
 
-				
 
 			}
 
@@ -105,7 +101,7 @@ public class HardwareView extends HardwareDesign implements View {
 			public void valueChange(ValueChangeEvent event) {
 				Notification.show("Attention! Sildervalue has changed to " + event.getProperty().getValue());
 
-				
+
 			}
 
 		});
@@ -123,7 +119,6 @@ public class HardwareView extends HardwareDesign implements View {
 			public void valueChange(ValueChangeEvent event) {
 				Notification.show("Attention! Sildervalue has changed to " + event.getProperty().getValue());
 
-				
 
 			}
 
@@ -146,7 +141,7 @@ public class HardwareView extends HardwareDesign implements View {
 				ConfirmDialog.show(UI.getCurrent(), "Changes will have big effect on the Database, please contact the IT manager! Press cancel to undo", new ConfirmDialog.Listener() {
 
 					/**
-					 * 
+					 *
 					 */
 					private static final long serialVersionUID = 1L;
 
@@ -154,50 +149,37 @@ public class HardwareView extends HardwareDesign implements View {
 					@Autowired
 					@Override
 					public void onClose(ConfirmDialog dialog) {
-						
+
 						// TODO Auto-generated method stub
 						if (dialog.isConfirmed()) {
 							// Confirmed to continue
 							// DO STUFF
-							beanItemContainer =  (BeanItemContainer<CpuConfigBean>) grid.getContainerDataSource();
-							CpuConfigBean cpuBean = new CpuConfigBean("init","init","init");
-							for( Iterator<CpuConfigBean> i = beanItemContainer.getItemIds().iterator(); i.hasNext();){
-								
-								
-								//get the current item identifier , which is an Integer
-								
-								CpuConfigBean iid=  i.next();
-						
-								
-								//now get the actual item from the container
-								
-								Item item = beanItemContainer.getItem(iid);
-								System.out.println("iid = "+ item.toString());
-								
-								System.out.println("cardnumber = "+ item.getItemProperty("cardNumber").getValue());
-								System.out.println("ioname = "+ item.getItemProperty("ioName").getValue());
-								System.out.println("ionumber = "+ item.getItemProperty("ioNumber").getValue());
-								
-								
-								cpuBean.setCardNumber((String)item.getItemProperty("cardNumber").getValue()) ;   
-								cpuBean.setIoName((String)item.getItemProperty("ioName").getValue()) ; 
-								cpuBean.setIoNumber((String)item.getItemProperty("ioNumber").getValue()) ; 
-								
-								System.out.println("params are written in bean");
-								
-								CpuConfigBean test = reposi.save(cpuBean);
-								Assert.assertNotNull(test.getId());
-								
-								System.out.println("params are written to database");
+							final Container.Indexed dataSource = grid.getContainerDataSource();
+							final Collection<CpuConfigBean> itemIds = (Collection<CpuConfigBean>) dataSource.getItemIds();
+							if (itemIds != null) {
+								for (CpuConfigBean iid : itemIds) {
+
+
+									System.out.println("iid = " + iid.toString());
+
+									System.out.println("Cardnumber: " + iid.getCardNumber());
+									System.out.println("ioName: " + iid.getIoName());
+									System.out.println("ioNumber: " + iid.getIoNumber());
+
+
+									System.out.println("params are written in bean");
+
+									CpuConfigBean test = reposi.save(iid);
+
+									System.out.println("params are written to database. (ID: " + test.getId() + ")");
+								}
 							}
-						
-							
 						} else {
 							// User did not confirm
 							// CANCEL STUFF
 						}
-						
-						
+
+
 					}
 
 				});
@@ -206,14 +188,14 @@ public class HardwareView extends HardwareDesign implements View {
 		});
 
 		configurationButton.addClickListener(new Button.ClickListener() {
-			
+
 			@Override
 			public void buttonClick(ClickEvent event) {
 				// TODO Auto-generated method stub
-				
+
 				for (double i = 0; i < digoutSlider.getValue(); i++) {
-					
-					
+
+
 					beanItemContainer.addBean(new CpuConfigBean("digital out card " + i, "output 1", "name"));
 					beanItemContainer.addBean(new CpuConfigBean("digital out card " + i, "output 2", "name"));
 					beanItemContainer.addBean(new CpuConfigBean("digital out card " + i, "output 3", "name"));
@@ -222,9 +204,9 @@ public class HardwareView extends HardwareDesign implements View {
 					beanItemContainer.addBean(new CpuConfigBean("digital out card " + i, "output 6", "name"));
 					beanItemContainer.addBean(new CpuConfigBean("digital out card " + i, "output 7", "name"));
 					beanItemContainer.addBean(new CpuConfigBean("digital out card " + i, "output 8", "name"));
-					
+
 				}
-				
+
 				for (double i = 0; i < diginSlider.getValue(); i++) {
 					beanItemContainer.addBean(new CpuConfigBean("digital in card " + i, "input 1", "name"));
 					beanItemContainer.addBean(new CpuConfigBean("digital in card " + i, "input 2", "name"));
@@ -234,9 +216,9 @@ public class HardwareView extends HardwareDesign implements View {
 					beanItemContainer.addBean(new CpuConfigBean("digital in card " + i, "input 6", "name"));
 					beanItemContainer.addBean(new CpuConfigBean("digital in card " + i, "input 7", "name"));
 					beanItemContainer.addBean(new CpuConfigBean("digital in card " + i, "input 8", "name"));
-					
+
 				}
-				
+
 				for (double i = 0; i < anainpSlider.getValue(); i++) {
 					beanItemContainer.addBean(new CpuConfigBean("analog in card " + i, "input 1", "name"));
 					beanItemContainer.addBean(new CpuConfigBean("analog in card " + i, "input 2", "name"));
@@ -245,17 +227,17 @@ public class HardwareView extends HardwareDesign implements View {
 //					
 
 				}
-				
+
 				for (double i = 0; i < anaoutputSlider.getValue(); i++) {
 					beanItemContainer.addBean(new CpuConfigBean("analog out card " + i, "output 1", "name"));
 //					
 
 
 				}
-				
+
 				grid.setContainerDataSource(beanItemContainer);
-		
-				
+
+
 			}
 		});
 
@@ -265,7 +247,7 @@ public class HardwareView extends HardwareDesign implements View {
 	@Override
 	public void enter(ViewChangeEvent event) {
 
-		
+
 		//BeanItemContainer container = new BeanItemContainer(CpuConfigBean.class);
 		//container = (BeanItemContainer) repo.findAll();
 		//grid.setContainerDataSource(container);
